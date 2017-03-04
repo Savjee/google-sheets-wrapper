e23d59ea5fd59e0498556b340048510056a01802
Xavier Decuyper <hi@savjee.be>
2017-03-04 10:45:50 +0100

    Added dummy test file (checking Travis)

---
+++ tests/GoogleSheetTest.ts
@@ -0,0 +1,16 @@
+import "mocha";
+
+describe('GoogleSheet test', () => {
+
+    describe('Constructor', () => {
+
+        it('Should fail with a wrong spreadsheet id', (done) => {
+            done();
+        });
+
+        it('Should fail with a bad range', (done) => {
+            done();
+        });
+        
+    });
+});
